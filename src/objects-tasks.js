61d867696731568314b99d18452713b3c2aa2eb7
Zakhar Pochuvalov <94977621+sobselena@users.noreply.github.com>
2025-11-24 19:03:48 +0300

    feat: solve makeWord task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -144,8 +144,20 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const len = Object.values(lettersObject).reduce((acc, arr) => {
+    return acc + arr.length;
+  }, 0);
+  const result = Array.from({ length: len }, () => {
+    return undefined;
+  });
+  Object.entries(lettersObject).forEach(([letter, indexes]) => {
+    indexes.forEach((index) => {
+      result[index] = letter;
+    });
+  });
+
+  return result.join('');
 }
 
 /**
